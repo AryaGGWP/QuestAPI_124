144de6e1a375d3d19477c720a530772900a6abb8
AryaGGWP <arya.lampung.2016@gmail.com>
2024-12-28 22:16:45 +0700

    menambahkan data class InsertUiState dan InsertUiEvent serta membuat class InsertViewModel

--- app/src/main/java/com/example/pertemuan12/ui/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/pertemuan12/ui/viewmodel/InsertViewModel.kt
@@ -1,12 +1,39 @@
 package com.example.pertemuan12.ui.viewmodel
 
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.pertemuan12.repository.MahasiswaRepository
+import kotlinx.coroutines.launch
 
 class InsertViewModel(private val mhs: MahasiswaRepository): ViewModel() {
     var uiState by mutableStateOf(InsertUiState())
         private set
 
-    fun updateInsertMhsState(insert)
+    fun updateInsertMhsState(insertUiEvent.InsertUiEvent) {
+        uiState = InsertUiState(insertUiEvent = insertUiEvent)
+    }
+    suspend fun insertMhs(){
+        viewModelScope.launch {
+            try {
+                mhs.insertMahasiswa(uiState.insertUiEvent.toMhs())
+            }catch (e:Exception){
+                e.printStackTrace()
+            }
+        }
+    }
 }
+
+data class InsertUiState(
+    val InsertUiEvent: InsertUiEvent = InsertUiEvent()
+)
+data class InsertUiEvent(
+    val nim: String = "",
+    val nama: String = "",
+    val alamat: String = "",
+    val jenisKelamin: String = "",
+    val kelas: String = "",
+    val angkatan: String = "",
+)
