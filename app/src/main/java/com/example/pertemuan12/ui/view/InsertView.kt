3aa8fc5573bd7276ccb978f6f32d22541f0136ed
AryaGGWP <arya.lampung.2016@gmail.com>
2024-12-29 14:18:08 +0700

    add function EntryMhsScreen sbg tampilan Entry

--- app/src/main/java/com/example/pertemuan12/ui/view/InsertView.kt
+++ app/src/main/java/com/example/pertemuan12/ui/view/InsertView.kt
@@ -1,9 +1,23 @@
 package com.example.pertemuan12.ui.view
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.pertemuan12.DestinasiNavigasi
+import com.example.pertemuan12.ui.viewmodel.InsertViewModel
 
 object DestinasiEntry : DestinasiNavigasi{
     override val route = "item_entry"
     override val titleRes = "Entry Mhs"
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EntryMhsScreen(
+    navigateBack: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: InsertViewModel = viewModel(factory = PenyediaViewModel.Factory)
+){
+
+}
