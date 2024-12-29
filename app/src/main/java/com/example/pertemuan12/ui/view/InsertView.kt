62ad0e0004893b3f732cbc57dbb103a1d073f00b
AryaGGWP <arya.lampung.2016@gmail.com>
2024-12-29 14:15:31 +0700

    memanggil DestinasiNavigasi

--- app/src/main/java/com/example/pertemuan12/ui/view/InsertView.kt
+++ app/src/main/java/com/example/pertemuan12/ui/view/InsertView.kt
@@ -1,2 +1,9 @@
 package com.example.pertemuan12.ui.view
 
+import com.example.pertemuan12.DestinasiNavigasi
+
+object DestinasiEntry : DestinasiNavigasi{
+    override val route = "item_entry"
+    override val titleRes = "Entry Mhs"
+}
+
