11c5d044ba291f84e23388ec61192d3e32b8a389
AryaGGWP <arya.lampung.2016@gmail.com>
2024-12-30 07:19:35 +0700

    update directory MahasiwaApplications

--- app/src/main/java/com/example/pertemuan12/MahasiswaApplications.kt
+++ app/src/main/java/com/example/pertemuan12/MahasiswaApplications.kt
@@ -1,8 +1,8 @@
 package com.example.pertemuan12
 
 import android.app.Application
-import com.example.pertemuan12.repository.AppContainer
-import com.example.pertemuan12.repository.MahasiswaContainer
+import com.example.pertemuan12.di.AppContainer
+import com.example.pertemuan12.di.MahasiswaContainer
 
 class MahasiswaApplications:Application(){
     lateinit var container: AppContainer
