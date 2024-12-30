83ab5f6e34c22a50c0fae3170e19ea2fe5ded16b
AryaGGWP <arya.lampung.2016@gmail.com>
2024-12-30 07:19:00 +0700

    add import yang dibutuhkan

--- app/src/main/java/com/example/pertemuan12/di/MahasiswaContainer.kt
+++ app/src/main/java/com/example/pertemuan12/di/MahasiswaContainer.kt
@@ -1,5 +1,7 @@
-package com.example.pertemuan12.repository
+package com.example.pertemuan12.di
 
+import com.example.pertemuan12.repository.MahasiswaRepository
+import com.example.pertemuan12.repository.NetworkKontakRepository
 import com.example.pertemuan12.service.MahasiswaService
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import kotlinx.serialization.json.Json
@@ -10,7 +12,7 @@ interface AppContainer{
     val kontakRepository: MahasiswaRepository
 }
 
-class MahasiswaContainer : AppContainer{
+class MahasiswaContainer : AppContainer {
     private val baseUrl = "http://localhost/umyTI/"
     private val json = Json { ignoreUnknownKeys = true }
     private val retrofit: Retrofit = Retrofit.Builder()
